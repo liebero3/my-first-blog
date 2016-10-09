5183dafb51d817cf8d3d75cfdf12411642fe090f
Till Lieber <liebero3@gmail.com>
2016-10-09 18:11:52 +0200

    navbar changed

--- myside3/blog/views.py
+++ myside3/blog/views.py
@@ -1,8 +1,17 @@
+#def homepage(request):
+#    if request.user.is_anonymous():
+#
+#    else:
+
+
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import Group
 
 # Create your views here.
+#@user_passes_test(lambda u: Group.objects.get(name='EFPHGK2') in u.groups.all())
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
